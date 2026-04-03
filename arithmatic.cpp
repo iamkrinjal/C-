5e0883c0d380b7739a3588563e90fe5e3442c94b
iamkrinjal <krinjalmatekar3@gmail.com>
2026-04-03 19:44:25 +0530

    add

---
+++ arithmatic.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int num1=3;
+int num2=5;
+cout<<num1+num2<<endl;
+cout<<num1-num2<<endl;
+cout<<num1*num2<<endl;
+cout<<num1/num2<<endl;
+return 0;
+
+
+
+}
---
+++ ternaryoperator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks;
+    cin>>marks;
+    if(marks>50){
+        cout<<"pass";
+        
+    }
+    else{
+        cout<<"fail";
+    }
+ marks>50?cout<<"pass":cout<<"fail";
+ 
+}
