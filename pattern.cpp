5ed390e3aa932981a182865932ee621a3508fdc2
iamkrinjal <krinjalmatekar3@gmail.com>
2026-04-05 21:01:27 +0530

    aa

---
+++ pattern.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=3;i<=5;i++){
+        cout<<"*"<<endl;
+        for(int j=1;j<=2;j++){
+            cout<<"*";
+        }
+    }
+
+    
+
+    return 0;
+}
